7fcfdba2a0406cbfc25c64f81df52d678551920f
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-05-23 16:44:02 +0900

    banner & loading spinner

--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -3,16 +3,34 @@ import api from "../api";
 const API_KEY = process.env.REACT_APP_API_KEY;
 function getMovies() {
   return async (dispatch) => {
-    const popularMovieApi = await api.get(
-      `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-    );
-    // let url = `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`
-    // let response = await fetch (url)
-    // let data = await response.json()
-
-    // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`
-    // let response = await fetch (url)
-    // let data = await response.json()
+    try {
+      dispatch({ type: "GET_MOVIES_REQUEST" });
+      const popularMovieApi = api.get(
+        `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      const topRatedApi = api.get(
+        `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      const upComingApi = api.get(
+        `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
+        popularMovieApi,
+        topRatedApi,
+        upComingApi,
+      ]);
+      dispatch({
+        type: "GET_MOVIES_SUCCESS",
+        payload: {
+          popularMovies: popularMovies.data,
+          topRatedMovies: topRatedMovies.data,
+          upComingMovies: upComingMovies.data,
+        },
+      });
+    } catch (error) {
+      //에러핸들링 하는 곳
+      dispatch({ type: "GET_MOVIES_FAILURE" });
+    }
   };
 }
 
