a3b84557d5e16ed217e837e31e1af5df639396b2
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-05-29 17:55:46 +0900

    movie card

--- src/redux/reducers/movieReducer.js
+++ src/redux/reducers/movieReducer.js
@@ -3,6 +3,7 @@ let initialState = {
   topRatedMovies: {},
   upComingMovies: {},
   loading: true,
+  genreList: [],
 };
 function movieReducer(state = initialState, action) {
   let { type, payload } = action;
@@ -18,6 +19,7 @@ function movieReducer(state = initialState, action) {
         popularMovies: payload.popularMovies,
         topRatedMovies: payload.topRatedMovies,
         upComingMovies: payload.upComingMovies,
+        genreList: payload.genreList,
         loading: false,
       };
     case "GET_MOVIES_FAILURE":
